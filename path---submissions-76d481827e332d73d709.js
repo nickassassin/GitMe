179522e7de14d7695e45856a75f6cf81b991bef6
Musthaq Ahamad <musthu.gm@gmail.com>
2018-06-29 23:28:20 +0530

    Updates

---
+++ path---submissions-76d481827e332d73d709.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xd6f4e5b44bd3],{323:function(a,e){a.exports={data:{allMarkdownRemark:{edges:[{node:{frontmatter:{username:"haxzie",fullname:"Musthaq Ahamad"}}},{node:{frontmatter:{username:"rumaan",fullname:"Rumaan Khalander"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---submissions-76d481827e332d73d709.js.map
